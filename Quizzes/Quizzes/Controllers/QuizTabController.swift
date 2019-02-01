cdab2d85c7418d12797e71d92b4399ec18ff57d9
alysonabril <alysonabril@pursuit.org>
2019-02-01 12:42:13 -0500

    deleted storyboard and created controllers

---
+++ Quizzes/Quizzes/Controllers/QuizTabController.swift
@@ -0,0 +1,18 @@
+//
+//  QuizTabController.swift
+//  Quizzes
+//
+//  Created by Alyson Abril on 2/1/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class QuizTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Quizzes/Quizzes/Controllers/QuizzesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizzesViewController.swift
+//  Quizzes
+//
+//  Created by Alyson Abril on 2/1/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class QuizzesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
+
+//must be programmatic
+/*
+ QuizCell
+ QuizDetailView
+ SearchCell
+ SearchQuizzesView
+ */
